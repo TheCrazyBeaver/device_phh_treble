fb2aec2ca846d6a3c55150c9c0394ab5c7d99593
TheCrazyBeaver <47144753+TheCrazyBeaver@users.noreply.github.com>
2019-01-30 23:09:59 +0100

    Update pixel.mk

--- pixel.mk
+++ pixel.mk
@@ -1,6 +1,5 @@
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
-$(call inherit-product, vendor/codefox/config/common.mk) 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk) 
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_GAPPS_ARCH := arm64
